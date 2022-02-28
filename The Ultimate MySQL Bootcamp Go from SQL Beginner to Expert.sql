1fc0de6b90ccddbff9dff4ee8fcdf1e8ac3d2a9a
Saheen Ahamed <sah.96ah@gmail.com>
2022-02-28 12:53:50 +0530

    learned update command

--- The Ultimate MySQL Bootcamp Go from SQL Beginner to Expert.sql
+++ The Ultimate MySQL Bootcamp Go from SQL Beginner to Expert.sql
@@ -96,3 +96,113 @@
 --     age int default 1);
 -- desc cats2;
 -- drop table cats2;
+
+-- KEYS
+-- PRIMARY KEYS - A UNIQUE IDENTIFIER
+-- create table unique_cats (
+-- 	cat_id int not null, 
+--     name varchar(20),
+--     age int,
+--     primary key (cat_id));
+-- desc unique_cats;
+
+-- insert into unique_cats 
+-- 	(cat_id, name, age)
+--     values (1, "Briteny", 2);
+    
+-- insert into unique_cats
+-- 	(cat_id, name, age)
+--     values (2, "Louise", 3);
+
+-- insert into unique_cats
+-- 	(cat_id, name, age)
+--     values 
+-- 		(3, "James", 7),
+--         (4, "Velu", 1);
+
+-- select *
+-- from unique_cats;
+
+-- when you use a number as a primary key, you can use AUTO_INCREMENT
+-- create table unique_cats2(
+-- 	cat_id int not null auto_increment,
+--     name varchar(20),
+--     age int,
+--     primary key (cat_id));
+
+-- CHALLANGE
+-- create table employees (
+-- 	id int auto_increment not null primary key,
+--     first_name varchar(50) not null,
+--     last_name varchar(50) not null,
+--     middle_name varchar(50),
+--     age int not null,
+--     current_status varchar(20) not null default "employed");
+
+-- show columns from employees;
+
+-- CRUD - Create, Read, Update, Delete
+-- CREATE
+-- create table cats (
+-- 	id int auto_increment not null primary key,
+--     name varchar(255),
+--     breed varchar(255),
+--     age int);
+
+-- insert into cats (name, breed, age)
+-- values
+-- 	('Ringo', 'Tabby', 4),
+--     ('Cindy', 'Maine Coon', 10),
+--     ('Dubledore', 'Maine Coon', 11),
+--     ('Egg', 'Persian', 4),
+--     ('Misty', 'Tabby', 13),
+--     ('George Micheal', 'Ragdoll', 9),
+--     ('Jackson', 'Sphynx', 7);
+    
+-- READ
+-- SELECT COLUMNS
+
+-- ALL COLUMNS
+-- select * from cats;
+
+-- SPECIFIC COLUMNS
+-- select name, breed from cats;
+
+-- THE ORDER MAY CHANGE
+-- select breed, id, age, name 
+-- from cats;
+
+-- WHERE
+-- CONDITIONAL SELECT 
+-- select *
+-- from cats
+-- where age = 4;  
+
+-- select *
+-- from cats
+-- where 
+-- 	breed = "persian" or age = 4
+
+-- CHALLANGE
+-- select id, age
+-- from cats
+-- where 
+-- 	id = age;
+
+-- UPDATE 
+-- update cats 
+-- set name="Dumbledore"
+-- where name = "Dubledore";
+
+-- update cats
+-- set age = 14
+-- where name = "misty";
+
+-- CHALLANGE
+-- update cats
+-- set name="Jack"
+-- where name = "Jackson";
+
+-- update cats
+-- set breed = "British Shorthair"
+-- where breed = "Tabby" and name = "Ringo";
