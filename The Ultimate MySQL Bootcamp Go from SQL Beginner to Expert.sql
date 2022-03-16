a0e6a51969ddabe2e69aee70f9ca089122eaf3d6
Saheen Ahamed <saheen@Saheens-MacBook-Pro.local>
2022-03-16 22:12:07 +0530

    learned delete in CRUD | CRUD essential practice

--- The Ultimate MySQL Bootcamp Go from SQL Beginner to Expert.sql
+++ The Ultimate MySQL Bootcamp Go from SQL Beginner to Expert.sql
@@ -206,3 +206,76 @@
 -- update cats
 -- set breed = "British Shorthair"
 -- where breed = "Tabby" and name = "Ringo";
+
+-- DELETE
+-- DELETE A ROW
+-- delete from cats where name="egg";
+
+-- DELETE ALL CATS
+-- delete from cats;
+
+-- CONDITIONAL DELETE - (DELETE MULIPLE ROW SETS)
+-- delete from cats
+-- where age > 4;
+
+-- CHALLANGE ON CRUD
+-- create database shirts_db;
+
+-- use shirts_db;
+
+-- create table shirts (
+-- 	shirt_id int auto_increment not null primary key,
+--     article varchar(50) not null,
+-- 	color varchar(50) not null, 
+--     shirt_size varchar(2) not null, 
+--     last_worn int not null);
+    
+-- insert into shirts (article, color, shirt_size, last_worn) 
+-- values 
+-- 	('t-shirt', 'white', 'S', 10),
+--     ('t-shirt', 'green', 'S', 200),
+--     ('polo shirt', 'black', 'M', 10), 
+--     ('tank top', 'blue', 'S', 50),
+--     ('t-shirt', 'pink', 'S', 0),
+-- 	('polo shirt', 'red', 'M', 5),
+--     ('tank top', 'white', 'S', 200),
+--     ('tank top', 'blue', 'M', 15);
+    
+-- insert into shirts (article, color, shirt_size, last_worn)
+-- values ('t-shirt', 'blue', 'L', 4);
+
+-- select article, color 
+-- from shirts;
+
+-- select * from shirts where shirt_size = "M";
+
+-- update shirts
+-- set shirt_size= "L";
+
+-- select * from shirts
+-- where article= "polo shirt";
+
+-- update shirts
+-- set last_worn = 0
+-- where last_worn <= 15;
+
+-- update shirts
+-- set shirt_size = "XS", color = "off white"
+-- where color = "white";
+
+-- select * from shirts;
+   
+-- delete from shirts
+-- where last_worn >= 200;
+
+-- delete from shirts 
+-- where article = "tank top";
+
+-- delete from shirts;
+
+-- drop table shirts;
+    
+    
+
+
+
