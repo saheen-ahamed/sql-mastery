e25816402d9c85fc223165991b3323962a25d514
Saheen Ahamed <sah.96ah@gmail.com>
2022-04-28 13:16:15 +0530

    oracle db basics

--- OracleDB/basics.sql
+++ OracleDB/basics.sql
@@ -1,52 +1,202 @@
--- drop users;
--- drop projects;
--- drop project_user;
+-- CREATE TABLES
 
--- create table users(
---     user_id number,
---     username varchar(50 char),
---     constraint users_pk primary key (user_id));
-    
--- create table projects(
---     project_id number,
---     project_name varchar(50 char),
---     creator number,
---     constraint project_pk primary key (project_id),
---     constraint project_user_fk foreign key (creator) references users (user_id)
---     on delete cascade);
+-- create table DEPARTMENTS (  
+--   deptno        number,  
+--   name          varchar2(50) not null,  
+--   location      varchar2(50),  
+--   constraint pk_departments primary key (deptno)  
+-- );
 
--- COMPOSITE PRIMARY KEY
+-- create table EMPLOYEES (  
+--   empno             number,  
+--   name              varchar2(50) not null,  
+--   job               varchar2(50),  
+--   manager           number,  
+--   hiredate          date,  
+--   salary            number(7,2),  
+--   commission        number(7,2),  
+--   deptno           number,  
+--   constraint pk_employees primary key (empno),  
+--   constraint fk_employees_deptno foreign key (deptno) 
+--       references DEPARTMENTS (deptno)  
+-- );
 
--- create table project_user(
---     user_id not null references users (user_id),
---     project_id not null references projects (project_id),
---     constraint project_user_pk primary key (user_id, project_id));
+-- TRIGGERS
 
--- INSERT
+-- create or replace trigger  DEPARTMENTS_BIU
+--     before insert or update on DEPARTMENTS
+--     for each row
+-- begin
+--     if inserting and :new.deptno is null then
+--         :new.deptno := to_number(sys_guid(), 
+--           'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+--     end if;
+-- end;
+-- /
 
--- insert into users (user_id, username) values (1, 'Saheen');
--- insert into users (user_id, username) values (2, 'Kumar');
+-- create or replace trigger EMPLOYEES_BIU
+--     before insert or update on EMPLOYEES
+--     for each row
+-- begin
+--     if inserting and :new.empno is null then
+--         :new.empno := to_number(sys_guid(), 
+--             'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+--     end if;
+-- end;
+-- /
 
--- MULTIPLE INSERTS
+-- INSERT DATA
 
--- insert all
---     into users (user_id, username) values (3, 'Khan')
---     into users (user_id, username) values (4, 'Kamal')
---     select 1 from dual;
+-- insert into departments (name, location) values
+--   ('Finance','New York');
+
+-- insert into departments (name, location) values
+--   ('Development','San Jose');
+   
+-- insert into EMPLOYEES (
+--     name, 
+--     job, 
+--     salary, 
+--     deptno) 
+-- values(
+--     'Sam Smith',
+--     'Programmer',
+--     5000, 
+--     (select deptno from departments where name = 'Development')
+-- );
+
+-- insert into EMPLOYEES 
+--   (name, job, salary, deptno) 
+--   values
+--   ('Mara Martin','Analyst', 
+--   6000, 
+--   (select deptno 
+--   from departments 
+--   where name = 'Finance'));
+
+-- insert into EMPLOYEES 
+--   (name, job, salary, deptno) 
+--   values
+--   ('Yun Yates','Analyst', 
+--   5500, 
+--   (select deptno 
+--   from departments 
+--   where name = 'Development'));
 
 
--- insert into projects (project_id, project_name, creator) values (1, 'pygame', 1);
+-- INDEXING 
 
--- insert into project_user (user_id, project_id) values (1, 1);
+-- RETRIVE THE INDEXES IF EXISTS
 
--- INDEXES
+-- select table_name "Table", 
+--       index_name "Index", 
+--       column_name "Column", 
+--       column_position "Position"
+-- from  user_ind_columns 
+-- where table_name = 'EMPLOYEES' or 
+--       table_name = 'DEPARTMENTS'
+-- order by table_name, column_name, column_position
 
--- create index project_creator_idx on projects (creator);
--- drop index project_creator_idx;
+-- INDEXING FOREIGN KEY
 
--- UPDATE
--- update users
--- set username= 'Kumar'
--- where username = 'Ahamed';
+-- create index employee_dept_no_fk_idx 
+-- on employees (deptno);
 
-select * from users;
+-- INDEXING THE FREQUENTY USED KEYS
+
+-- create unique index employee_ename_idx
+-- on employees (name);
+
+-- QUERY DATA FROM TABLES
+
+-- select * from employees
+
+-- JOINS
+
+-- IMPLICIT INNER JOIN
+
+-- select 
+--     e.name employee,
+--     d.name department,
+--     e.job,
+--     d.location
+-- from employees e, departments d
+-- where e.deptno = d.deptno
+-- order by e.name;
+
+-- EXPLICIT INNER JOIN
+-- select 
+--     e.name employee,
+--     d.name department,
+--     e.job,
+--     d.location
+-- from 
+--     employees e inner join departments d
+--     on e.deptno = d.deptno
+-- order by 1;
+
+-- ALTER TABLE
+
+-- alter table employees
+-- add country_code varchar2(2);
+
+-- DESCRIBE TABLE/ DATA DICTIONARY/ META DATA 
+
+-- select table_name, tablespace_name, status
+-- from user_tables
+-- where table_name = 'EMPLOYEES';
+
+-- select column_id, column_name , data_type
+-- from user_tab_columns
+-- where table_Name = 'EMPLOYEES'
+-- order by column_id;
+
+-- UPDATE DATA
+
+-- update employees
+-- set job = 'Data Engineer'
+-- where name = 'Sam Smith';
+
+-- update employees
+-- set country_code = 'US';
+
+-- DATA AGGERATION  
+
+-- select max(salary) max_salary
+-- from employees;
+
+-- select count(*) total_department
+-- from departments;
+
+-- select 
+--       count(*) employee_count,
+--       sum(salary) total_salary,
+--       sum(commission) total_commission,
+--       min(salary + nvl(commission,0)) min_compensation,
+--       max(salary + nvl(commission,0)) max_compensation
+-- from employees;
+
+-- TABLE COMPRESSION
+-- alter table EMPLOYEES compress for oltp; 
+-- alter table DEPARTMENTS compress for oltp; 
+
+-- DECOMPRESSION
+-- alter table EMPLOYEES nocompress;
+
+-- DELETE DATA ROWS
+-- delete from employees
+-- where name = 'Yun Yates';
+
+-- DROP TABLES
+-- drop table employees cascade constraint;
+-- drop table departments cascade constraint;
+
+-- RECYCLE DELETED DATA
+
+-- select object_name, original_name, type, can_undrop, can_purge
+-- from recyclebin;
+
+-- UNDROP TABLE USE FLASHBACK COMMAND
+
+-- flashback table employees to before drop; 
+-- flashback table departments to before drop;
