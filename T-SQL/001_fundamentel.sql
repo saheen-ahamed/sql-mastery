290e9c7a1b49546663a610b51935842027f2e666
Saheen Ahamed <sah.96ah@gmail.com>
2022-05-16 08:17:00 +0530

    TSQL - stored procedures and functions

--- T-SQL/001_fundamentel.sql
+++ T-SQL/001_fundamentel.sql
@@ -148,3 +148,63 @@ begin
     close employee_cursor;
     dealloate employee_cursor;
 end;
+
+-- STORED PROCEDURES
+
+create or alter procedure salary_increment @deptId int as
+begin
+    declare @id int;
+    declare @ename varchar(100);
+    declare @sal numeric(8, 2);
+
+    declare employee_cursor cursor for
+        select eno, ename, salary from employee where dept = 30;
+    open employee_cursor;
+    fetch next from employee_cursor into @id, @ename, @sal;
+    while @@fetch_status = 0
+    begin
+      if @sal < 30000
+      begin
+        set @sal = @sal + 5000;
+        update employee set salary = @sal where eno = @id;
+      end;
+      print @name + ' earns ' + cast(@sal as varchar);
+      fetch next from employee_cursor into @id, @ename, @sal;
+    end;
+    close employee_cursor;
+    dealloate employee_cursor;
+end;
+
+execute salary_increment 20;
+
+-- OUT/OUTPUT paramater (get value for formal parameter)
+
+create proc get_emp @id int, @ename varchar(100) output, @sal numeric(8,2) output as
+begin
+  select
+    @ename = ename,
+    @sal = salary
+  from employee
+  where eno = @id;
+end;
+
+begin
+  declare @v_name varchar(100);
+  declare @v_salary numeric(8,2);
+
+  exec get_emp 5, @v_name out, @v_salary out;
+  print @v_name + ' earns ' + cast(@v_salary as varchar);
+end;
+
+-- STORED FUNCTIONS
+
+-- Functions can be used with select, where and having clause
+-- Return a value
+
+create or alter function get_tex(@amount numeric(8, 2)) returns numeric (8, 2) as
+begin
+  return @amount * 0.1;
+end;
+
+select eno, ename, salary, get_tex(salary) as tex_amount
+from employee;
